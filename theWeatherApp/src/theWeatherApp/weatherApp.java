6cf1b1c0b1e4da77bf6cf67ce049791712642e11
Victor Peraza <69927217+Victor-Peraza@users.noreply.github.com>
2020-11-11 16:13:04 -0800

    Victor - Hello World

--- theWeatherApp/src/theWeatherApp/weatherApp.java
+++ theWeatherApp/src/theWeatherApp/weatherApp.java
@@ -2,6 +2,7 @@ package theWeatherApp;
 
 public class weatherApp {
     public static void main(String args[]){
-        System.out.println("Hello");
+
+        System.out.println("Hello World");
     }
 }
